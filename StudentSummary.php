42533ed6cb7d69c0d64b69916454e4281bfa1000
William Klausmeyer <wklausmeyer@yahoo.com>
2015-03-03 12:02:53 -0600

    Aesthetic changes

--- StudentSummary.php
+++ StudentSummary.php
@@ -39,13 +39,7 @@ if ( isset($_POST['SID']) ) {
 <?php
 if ( isset($_POST['SID']) && $ErrorString == "" ) {
 	echo '<h1>Student Summary: ' . $NameFL . '</h1>
-<h3>Year: ' . $Year . '</h3>
-<h3>Events</h3>
 ';
-foreach($Events as $key => $value) {
-	echo $key . "<br>
-";
-}
 } elseif ( isset($_POST['SID']) ) {
 	echo '<h1>Student Summary Page</h1>
 <h3>' . $ErrorString . '</h3>
@@ -55,12 +49,20 @@ foreach($Events as $key => $value) {
 }
 ?>
 <form id="StudentSelect" action="StudentSummary.php" method="post">
-<?php echo Students(0,"StudentSelect"); ?>
+Select another student: <?php echo Students(0,"StudentSelect"); ?>
 <input type="submit" value="select">
 </form>
+<div style="display: float; float: left; padding-right: 10; width: 18%">
 <?php
 if ( isset($_POST['SID']) && $ErrorString == "" ) {
-	$query = mysql_query("select distinct EName from Events, Results where Results.EID = Events.EID and Results.SID = '" . $_POST['SID'] . "' and Results.State = 1 order by EName;");
+	echo '<h3>Year: ' . $Year . '</h3>
+<h3>Events</h3>
+';
+	foreach($Events as $key => $value) {
+		echo $key . "<br>
+";
+	}
+	$query = mysql_query("select distinct EName from Events, Results where Results.EID = Events.EID and (Results.SID='" . $_POST['SID'] . "' or Results.SID2='" . $_POST['SID'] . "') and Results.State = 1 order by EName;");
 	echo '<h3>State Qualifications</h3>
 ';
 	if (( mysql_errno() )) {
@@ -81,28 +83,17 @@ if ( isset($_POST['SID']) && $ErrorString == "" ) {
 ";
 	}
 	
-	echo "<h3>Progress</h3>
-<div id='visualization' style='width: 95%;'></div>
+	echo "</div>
+<div style='display: inline-block; width: 80%'>
+<div id='visualization' style='width: 100%;'></div></div>
 <script>
 function MakeGraph() {
 	var data = new google.visualization.DataTable();
 	data.addColumn('string','Tournament');
 ";
-	$query = mysql_query("select EName from Results, Events where (SID='" . $_POST['SID'] . "' or SID2='" . $_POST['SID'] . "') and Results.EID = Events.EID group by EName order by EName;");
-	if (( mysql_errno() )) {
-		$ErrorString =  "Error - MySQL error " . mysql_errno() . ": " . mysql_error() . ".";
-		echo "}</script>" . $ErrorString;
-		break;
-	}
-	$NumRows = mysql_num_rows($query);
-	$CurrentRow = 1;
-	$Events = array();
-	while ( $CurrentRow <= $NumRows ) {
-		$Data = mysql_fetch_assoc($query);
-		$Events[$Data['EName']] = $CurrentRow;
-		echo "data.addColumn('number', '" . $Data['EName'] . "');
+	foreach ( $Events as $key => $value ) {
+		echo "data.addColumn('number', '" . $key . "');
 ";
-		$CurrentRow++;
 	}
 	$query = mysql_query("select TName from Results, Tournaments where (SID='" . $_POST['SID'] . "' or SID2='" . $_POST['SID'] . "') and Results.TID = Tournaments.TID group by Date order by Date;");
 	if (( mysql_errno() )) {
@@ -120,7 +111,7 @@ function MakeGraph() {
 ";
 		$CurrentRow++;
 	}
-	$query = mysql_query("select EName, Date, avg(10 - PRanks/NumberRounds) as Ranks from Results, Tournaments, Events where Results.TID = Tournaments.TID and Results.EID = Events.EID and (Results.SID='" . $_POST['SID'] . "' or Results.SID2='" . $_POST['SID'] . "') group by Date, EName order by Date, EName;");
+	$query = mysql_query("select EName, Date, avg(PRanks/NumberRounds) as Ranks from Results, Tournaments, Events where Results.TID = Tournaments.TID and Results.EID = Events.EID and (Results.SID='" . $_POST['SID'] . "' or Results.SID2='" . $_POST['SID'] . "') group by Date, EName order by Date, EName;");
 	if (( mysql_errno() )) {
 		$ErrorString = "Error - MySQL error " . mysql_errno() . ": " . mysql_error() . ".";
 		echo $ErrorString;
@@ -141,7 +132,7 @@ function MakeGraph() {
 		$CurrentRow++;
 	}
 	echo "var chart = new google.visualization.LineChart(document.getElementById('visualization'));
-	chart.draw(data, {height: 500, vAxis: { viewWindowMode:'explicit', viewWindow: {min: 0, max: 9}}, interpolateNulls: true});
+	chart.draw(data, {height: 500, vAxis: { direction: -1, gridlines: {count: 10}, viewWindowMode:'explicit', viewWindow: {max: 0, min: 9}}, interpolateNulls: true});
 	}
 	google.setOnLoadCallback(MakeGraph);
 </script>";
