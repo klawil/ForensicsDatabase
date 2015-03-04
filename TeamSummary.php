7b1dbc98c00cb4947be88fa3da018702efd65e4c
William Klausmeyer <wklausmeyer@yahoo.com>
2015-03-03 21:17:33 -0600

    Added graph functionality

--- TeamSummary.php
+++ TeamSummary.php
@@ -57,7 +57,7 @@ if (( mysql_errno() )) {
 		$CurrentRow++;
 	}
 }
-echo "<div id='visualization' style='width: 99%'></div>
+echo "<div id='visualization' style='width: 99%; height: 100%;'></div>
 <script>
 function MakeGraph() {
 	var data = new google.visualization.DataTable();
@@ -67,12 +67,48 @@ foreach ( $Events as $key => $value ) {
 	echo "data.addColumn('number','" . $key . "');
 	";
 }
-$query = mysql_query("select TName from Results, Tournaments where Results.TID = Tournaments.TID group by Date order by Date;");
+$query = mysql_query("select TName, Date from Results, Tournaments where Results.TID = Tournaments.TID group by Date order by Date;");
 if (( mysql_errno() )) {
 	echo "Error - MySQL error " . mysql_errno() . ": " . mysql_error() . ".";
 } else {
-	
+	$CurrentRow = 1;
+	$NumRows = mysql_num_rows($query);
+	$Tournaments = array();
+	echo "data.addRows(" . $NumRows . ");
+	";
+	while ( $CurrentRow <= $NumRows ) {
+		$Data = mysql_fetch_assoc($query);
+		echo "data.setValue(" . ($CurrentRow - 1) . ",0,'" . $Data['TName'] . "');
+	";
+		$Tournaments[$Data['Date']] = ($CurrentRow - 1);
+		$CurrentRow++;
+	}
 }
+$query = mysql_query("select EName, Date, avg(PRanks/NumberRounds) as Ranks from Results, Tournaments, Events where Results.TID = Tournaments.TID and Results.EID = Events.EID group by Date, EName order by Date, EName;");
+if (( mysql_errno() )) {
+	echo "Error - MySQL error " . mysql_errno() . ": " . mysql_error() . ".";
+} else {
+	$Row = -1;
+	$Date1 = "";
+	$CurrentRow = 0;
+	$NumRows = mysql_num_rows($query);
+	while ( $CurrentRow < $NumRows ) {
+		$Data = mysql_fetch_assoc($query);
+		if ( $Data['Date'] != $Date1 ) {
+			$Date1 = $Data['Date'];
+			$Row++;
+		}
+		echo "data.setValue(" . $Row . "," . $Events[$Data['EName']] . "," . round($Data['Ranks'],2) . ");
+	";
+		$CurrentRow++;
+	}
+}
+echo "var chart = new google.visualization.LineChart(document.getElementById('visualization'));
+	chart.draw(data, {vAxis: { direction: -1, gridlines: {count: 7}, viewWindowMode:'explicit', viewWindow: {max: 0, min: 6}}, interpolateNulls: true});
+}
+google.setOnLoadCallback(MakeGraph);
+</script>
+";
 ?>
 </body>
 </html>
