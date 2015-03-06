c2d4b7c5be90f13b13e7cca75ddd311518fd0058
William Klausmeyer <wklausmeyer@yahoo.com>
2015-03-06 17:10:22 -0600

    Added shell for Tournament Editing

---
+++ TournamentEdit.php
@@ -0,0 +1,34 @@
+<?php
+include 'CommonFunctions.php';
+if ( isset($_POST['TID']) ) {
+	$WString = " where TID='" . $_POST['TID'] . "'";
+	
+}
+?>
+<html>
+<head>
+<title>Tournament Edit</title>
+<link rel="stylesheet" type="text/css" href="Styles.css">
+</head>
+<body>
+<h1>Tournament Edit</h1>
+<?php MakeHeader();
+if ( $GLOBALS['CanUserEdit'] != 1 ) {
+	echo '<h2>Authentication Error: You do not have the permission to access this page</h2>
+</body>
+</html>';
+	return 0;
+}
+$query = mysql_query("update Ballots set Rank=1 where RID=119 and Judge=1;");
+echo mysql_affected_rows();
+?>
+Select Tournament
+<div id="Tourneys"><?php echo Tournaments(0); ?> <input type="button" value="Select" onclick="MakePage();"></div><form id="EntryID" action="TournamentUpdate.php"><div id="TourneyID"></div>
+<script>
+function MakePage() {
+	TID = document.getElementById("Tournament").options[document.getElementById("Tournament").selectedIndex].value;
+	window.alert(TID);
+}
+</script>
+</body>
+</html>
