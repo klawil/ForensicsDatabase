fbad6bc384a19fc3895b50adeb9399feb339b128
William Klausmeyer <wklausmeyer@yahoo.com>
2015-03-09 22:07:52 -0500

    Added log of logging in and out

--- Login.php
+++ Login.php
@@ -16,6 +16,9 @@ if ( isset($_POST['UName']) ) {
 	if ( password_verify($_POST['PWord'], $data['password']) ) {
 		if ( SetAuthCookie($_POST['UName']) == 1 ) {
 			echo "true";
+			$myfile = fopen("/var/log/forensics/general.log","a");
+			fwrite($myfile,$_POST['UName'] . " logged in from IP " . $_SERVER['REMOTE_ADDR'] . " on " . date('Y-m-d') . " at " . date('H:i:s') . "\n");
+			fclose($myfile);
 		}
 	} else {
 		echo "Incorrect password";
@@ -25,6 +28,9 @@ if ( isset($_POST['UName']) ) {
 if ( isset($_POST['Logout']) ) {
 	unset($_COOKIE[$GLOBALS['CookieName']]);
 	setcookie($GLOBALS['CookieName'], NULL, 0, "/");
+	$myfile = fopen("/var/log/forensics/general.log","a");
+	fwrite($myfile,$GLOBALS['UserName'] . " logged out from IP " . $_SERVER['REMOTE_ADDR'] . " on " . date('Y-m-d') . " at " . date('H:i:s') . "\n");
+	fclose($myfile);
 	echo "true";
 }
 ?>
