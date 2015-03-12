8ffaaa143a7775a62d08761ba23132ede5ef6b1a
William Klausmeyer <wklausmeyer@yahoo.com>
2015-03-12 08:59:02 -0500

    Framework for issue reporting

---
+++ IssueReport.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<html>
+<head><title>Issue Reporting</title>
+<link rel="stylesheet" type="text/css" href="Styles.css">
+</head>
+<body>
+<h1><div id="Header" style="width: 100%;">Report an Issue with the Website</div></h1>
+<?php MakeHeader(); ?>
+<form id="IssueSubmit" name="IssueSubmit" action="IssueReport.php" method="post">
+
+</form>
+</body>
+</html>
