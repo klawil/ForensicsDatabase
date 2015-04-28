510f7bbe985e8b2819820686bcf75ca4264ca408
klawil <wklausmeyer@yahoo.com>
2015-04-28 09:58:07 -0500

    Changed file names and added files for code testing

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+/*include 'CommonFunctions.php';
+$Ballots = ['test' => ['RID' => '1', 'Round' => '01', 'Judge' => '1', 'ElimLevel' => '5', 'Rank' => 1],
+'other' => ['RID' => '1', 'Round' => '1', 'Judge' => '1', 'ElimLevel' => '5', 'Rank' => 1, 'Qual' => 'no']];
+$Array = InsertBallots($Ballots);
+foreach ( $Array as $key => $Value ) {
+	echo $key . ': ' . $Value . '<br>
+';
+}*/
+if ( "This is a string" ) {
+	echo "Yes";
+} else {
+	echo "No";
+}
+?>
