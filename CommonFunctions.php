92a334aec3ad36653e2f95d29ce88a43f26b23e1
klawil <wklausmeyer@yahoo.com>
2015-04-29 16:39:52 -0500

    Gutted even more

--- CommonFunctions.php
+++ CommonFunctions.php
@@ -4,36 +4,6 @@ $GLOBALS['CookieName'] = 'forensics_db_auth_token'; // Name of the cookie used t
 $GLOBALS['SecretWord'] = 'ForensicsSECRET'; // MD5'd with username to cookie name
 $GLOBALS['UserName'] = ''; // Stores the username
 $GLOBALS['CanUserEdit'] = 0; // Stores the admin ability of the user
-function CheckAuthorization() {
-	// Checks if the user has cookie authorization
-	if ( isset($_COOKIE[$GLOBALS['CookieName']]) ) { do {
-		$Array = explode(',',$_COOKIE[$GLOBALS['CookieName']],2);
-		$GLOBALS['UserName'] = $Array[0];
-		$Cookie = $Array[1];
-		$query = mysqli_query($GLOBALS['DBConn'], "select cookie, cookieExp, CanMod, concat(LName, ', ', FName) as Name from users where UName='" . $GLOBALS['UserName'] . "';");
-		if ( !$query ) {
-			break;
-		}
-		$Data = mysqli_fetch_assoc($query);
-		if ( $Cookie != $Data['cookie'] ) {
-			setcookie($GLOBALS['CookieName'], "", time() - 3600);
-			$GLOBALS['UserName'] = "";
-			break;
-		} elseif ( time() > $Data['cookieExp'] ) {
-			setcookie($GLOBALS['CookieName'], "", time() - 3600);
-			$GLOBALS['UserName'] = "";
-			break;
-		}
-		$GLOBALS['UserName'] = $Data['Name'];
-		$GLOBALS['CanUserEdit'] = $Data['CanMod'];
-	} while (false); }
-	$myfile = fopen("/var/log/forensics/general.log","a");
-	if ( $GLOBALS['UserName'] != "" ) {
-		fwrite($myfile, "User " . $GLOBALS['UserName'] . " from ");
-	}
-	fwrite($myfile, "IP " . $_SERVER['REMOTE_ADDR'] . " accessed " . basename($_SERVER['PHP_SELF']) . " on " . date('Y-m-d') . " at " . date('H:i:s') . "\n");
-	fclose($myfile);
-}
 function SetAuthCookie($UN) {
 	// Sets an authoriztion cookie
 	// $UN - the username to set the cookie for
