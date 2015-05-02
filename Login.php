d70465f5aad75e32e5dc25d11d9fcf9c77364b77
klawil <wklausmeyer@yahoo.com>
2015-05-02 18:11:54 -0500

    Testing code

--- Login.php
+++ Login.php
@@ -2,5 +2,9 @@
 require_once 'include.inc';
 if ( isset($_POST['Logout']) ) {
 	setcookie($GLOBALS['CookieName'],NULL,0,'/');
+	echo 'true';
+	return 0;
 }
+SetAuthCookie($_POST['UName'], $DBConn);
+echo 'true';
 ?>
