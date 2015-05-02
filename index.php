3bd3f1c0f1b4cbd2a44928e59efbb9e20ab09e43
klawil <wklausmeyer@yahoo.com>
2015-05-02 10:02:54 -0500

    Added debug code

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ $GLOBALS['PageName'] = $GLOBALS['DBData']['SchoolName'];
 require_once 'header.inc';
 ?>
 <h1>Site under construction</h1>
+<?php
+foreach ( $GLOALS['DBData'] as $Key => $Value ) {
+	echo $Key . '=>' . $Value . '<br>';
+}
+?>
 </body>
 </html>
