cd952083f7b1cb692015419c8201fda08b3c637c
klawil <wklausmeyer@yahoo.com>
2015-05-02 18:19:37 -0500

    Javascript code

--- Login.php
+++ Login.php
@@ -1,10 +1,23 @@
 <?php
 require_once 'include.inc';
+
+// Handle logging a user out
 if ( isset($_POST['Logout']) ) {
 	setcookie($GLOBALS['CookieName'],NULL,0,'/');
 	echo 'true';
 	return 0;
 }
-SetAuthCookie($_POST['UName'], $DBConn);
-echo 'true';
+
+// Handle logging a user in
+if ( isset($_POST['UName']) ) {
+	
+	// Check password is received
+	if ( !isset($_POST['PWord']) ) {
+		echo 'Password must be entered';
+		return 0;
+	}
+}
+
+// Any other action
+require_once '404.html';
 ?>
