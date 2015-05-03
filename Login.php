3c298720e81a698cb6765b2c4e03170140308572
klawil <wklausmeyer@yahoo.com>
2015-05-02 21:16:38 -0500

    Finished login script

--- Login.php
+++ Login.php
@@ -10,10 +10,38 @@ if ( isset($_POST['Logout']) ) {
 
 // Handle logging a user in
 if ( isset($_POST['UName']) ) {
+	// Global error variable
+	$IncorrectString = 'Incorrect username or password';
 	
 	// Check password is received
 	if ( !isset($_POST['PWord']) ) {
 		echo 'Password must be entered';
+		return 0;
+	}
+	
+	// Escape variables
+	$UN = MySQLEscape($_POST['UName'], $DBConn);
+	$PW = MySQLEscape($_POST['PWord'], $DBConn);
+	
+	// Get password hash and check username existence
+	$UserString = 'select password from Users where UName="' . $UN . '";';
+	$UserQuery = MySQLQuery($DBConn,$UserString);
+	if ( !$UserQuery['Result'] ) {
+		echo $UserQuery['Query'];
+		return 0;
+	} elseif ( mysqli_num_rows($UserQuery['Query']) != 1 ) {
+		echo $IncorrectString;
+		return 0;
+	}
+	$Data = mysqli_fetch_assoc($UserQuery['Query']);
+	if ( password_verify($PW,$Data['password']) ) {
+		if ( SetCookie($UN,$DBConn) == true ) {
+			echo 'true';
+			WriteLog($UN . ' logged in');
+			return 0;
+		}
+	} else {
+		echo $IncorrectString;
 		return 0;
 	}
 }
