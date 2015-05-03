889d67558490d4e9c44df6a3d929de41307d90bf
klawil <wklausmeyer@yahoo.com>
2015-05-03 12:55:00 -0500

    Added better layout to the page

--- ManageStudent.php
+++ ManageStudent.php
@@ -19,12 +19,18 @@ $NumRows = mysqli_num_rows($StudentQuery['Query']);
 $CurrentRow = 1;
 while ( $CurrentRow <= $NumRows ) {
 	$StudentData = mysqli_fetch_assoc($StudentQuery['Query']);
-	echo '<tr><td id="FName' . $StudentData['SID'] . '">' . $StudentData['FName'] . '</td><td id="LName' . $StudentData['SID'] . '">' . $StudentData['LName'] . '</td><td id="NYear' . $StudentData['SID'] . '">' . $StudentData['NoviceYear'] . '</td><td><input type="button" onclick="SubmitStudent(' . $StudentData['SID'] . ')"></td></tr>';
+	echo '<tr><td id="FName' . $StudentData['SID'] . '">' . $StudentData['FName'] . '</td><td><input type="text" id="LName' . $StudentData['SID'] . '" value="' . $StudentData['LName'] . '"></td><td>' . CreateSeasonList($DBConn,NULL,$StudentData['NoviceYear'],"NYear" . $StudentData['SID']) . '</td><td><input type="button" onclick="SubmitStudent(' . $StudentData['SID'] . ')" value="Save Changes"></td></tr>';
 	$CurrentRow++;
 }
 // LName FName NoviceYear SID
 ?>
-<tr><td><input type="text" id="FName" width="100%"></td><td><input type="text" id="LName" width="100%"></td><td><input type="text" id="NYear" width="100%"></td><td><input type="button" onclick="SubmitStudent()"></td></tr>
+<tr><td><input type="text" id="FName" width="100%"></td><td><input type="text" id="LName" width="100%"></td><td><?php echo CreateSeasonList($DBConn,NULL,NULL,'NYear');?></td><td><input type="button" onclick="SubmitStudent()" value="Create Student"></td></tr>
 </table>
+<script>
+function SubmitStudent(SID) {
+	SID = SID || -1;
+	
+}
+</script>
 </body>
 </html>
