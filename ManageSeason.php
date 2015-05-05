b2e267ffc1d4325264842aae544bd21e3b784506
klawil <wklausmeyer@yahoo.com>
2015-05-05 10:53:25 -0500

    Changed table names to be more descriptive and made associated changes in variable names

--- ManageSeason.php
+++ ManageSeason.php
@@ -3,7 +3,7 @@ require_once 'include.inc';
 $GLOBALS['PageName'] = 'Season Management';
 
 // Handle update
-if ( isset($_POST['YID']) ) {
+if ( isset($_POST['SeasonID']) ) {
 
 }
 
@@ -25,7 +25,7 @@ require_once 'header.inc';
 ?>
 <h3>Update, create, and delete seasons</h3>
 <table class="Table">
-<tr><th>Start Year</th><th>End Year</th><th></th></tr>
+<tr><th>Season Name</th><th>Start Year</th><th></th></tr>
 <?php
 // Set up loop variables
 $NumRows = mysqli_num_rows($SeasonQuery['Query']);
@@ -37,57 +37,62 @@ while ( $CurrentRow <= $NumRows ) {
 	$SeasonData = mysqli_fetch_assoc($SeasonQuery['Query']);
 	
 	// Add a row to the html table
-	echo '<tr><td><input type="number" id="StartYear' . $SeasonData['YID'] . '" value="' . $SeasonData['StartYear'] . '" onchange="ShowHideButton(' . $SeasonData['YID'] . ')"></td><td id="EndYear' . $SeasonData['YID'] . '">' . $SeasonData['EndYear'] . '</td><td class="ChangeCell" id="' . $SeasonData['YID'] . '"><input type="button" value="Save Changes" onclick="SubmitSeason(' . $SeasonData['YID'] . ')"></td></tr>';
+	echo '<tr><td><input type="text" id="SeasonName' . $SeasonData['SeasonID'] . '" value="' . $SeasonData['SeasonName'] . '" onchange="ShowHideButton(' . $SeasonData['SeasonID'] . ')"></td><td><input type="number" id="StartYear' . $SeasonData['SeasonID'] . '" value="' . $SeasonData['StartYear'] . '" onchange="ShowHideButton(' . $SeasonData['SeasonID'] . ')"></td><td class="ChangeCell" id="' . $SeasonData['SeasonID'] . '"><input type="button" value="Save Changes" onclick="SubmitSeason(' . $SeasonData['SeasonID'] . ')"></td></tr>';
 	$CurrentRow++;
 }
-// LName FName NoviceYear SID
 ?>
-<tr><td><input type="number" id="StartYear" onchange="ShowHideButton()"></td><td id="EndYear"></td><td class="ChangeCell" id="NewSeason"><input type="button" value="Save Changes" onclick="SubmitSeason()"></td></tr>
+<tr><td><input type="text" id="SeasonName" onchange="ShowHideButton()"></td><td><input type="number" id="StartYear" onchange="ShowHideButton()"></td><td class="ChangeCell" id="NewSeason"><input type="button" value="Save Changes" onclick="SubmitSeason()"></td></tr>
 </table>
 <script>
 function pad (str, max) {
   str = str.toString();
   return str.length < max ? pad("0" + str, max) : str;
 }
-function ShowHideButton(YID) {
-	// Set default YID
-	YID = YID || -1;
+function ShowHideButton(SeasonID) {
+	// Set default SeasonID
+	SeasonID = SeasonID || -1;
 		
 	// Set Element Names
-	StartElementName = "StartYear";
-	EndElementName = "EndYear";
-	ButtonElementName = "NewSeason";
-	if ( YID != -1 ) {
-		StartElementName = StartElementName + YID;
-		EndElementName = EndElementName + YID;
-		ButtonElementName = YID;
+	StartYearElement = "StartYear";
+	SeasonNameElement = "SeasonName";
+	ButtonElement = "NewSeason";
+	if ( SeasonID != -1 ) {
+		StartYearElement = StartYearElement + SeasonID;
+		SeasonNameElement = SeasonNameElement + SeasonID;
+		ButtonElement = SeasonID;
 	}
 	
-	// Get start year and set end year
-	StartYear = document.getElementById(StartElementName).value;
+	// Get start year
+	StartYear = document.getElementById(StartYearElement).value;
 	StartYear = pad(StartYear,4);
-	document.getElementById(StartElementName).value = StartYear;
-	document.getElementById(EndElementName).innerHTML = pad(parseInt(StartYear) + 1,4);
+	document.getElementById(StartYearElement).value = StartYear;
+	
+	// Get Season name
+	SeasonName = document.getElementById(SeasonNameElement).value;
 	
 	// Show save button if the value is changed
-	if ( document.getElementById(StartElementName).value != document.getElementById(StartElementName).defaultValue ) {
-		document.getElementById(ButtonElementName).style.display = "inline";
+	if ( document.getElementById(StartYearElement).value != document.getElementById(StartYearElement).defaultValue ) {
+		// Start Year is changed
+		document.getElementById(ButtonElement).style.display = "inline";
+	} else if ( document.getElementById(SeasonNameElement).value != document.getElementById(SeasonNameElement).defaultValue ) {
+		// Season name is changed
+		document.getElementById(ButtonElement).style.display = "inline";
 	} else {
 		document.getElementById(ButtonElementName).style.display = "none";
 	}
 }
-function SubmitSeason(YID) {
-	// Set default YID
-	YID = YID || -1;
+function SubmitSeason(SeasonID) {
+	// Set default SeasonID
+	SeasonID = SeasonID || -1;
 	
 	// Declare PostString
 	PostString = "";
 	
 	// Set Element name
 	StartElementName = "StartYear";
-	if ( YID != -1 ) {
-		StartElementName = StartElementName + YID;
-		PostString = "YID=" + YID + "&";
+	if ( SeasonID != -1 ) {
+		StartElementName = StartElementName + SeasonID;
+		PostString = "SeasonID=" + SeasonID + "&";
 	}
 	
 	// Get start year and add to PostString
