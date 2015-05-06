86dbdaba1e99c1c0b4c338b72014febd902aee2e
klawil <wklausmeyer@yahoo.com>
2015-05-06 17:42:08 -0500

    Modified setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-mysqluser="" # User to create database with
-mysqlpass="" # Password
-mysqlwebuser="" # Username for php user
-mysqlwebpass="" # Password for php user
-mysqldbname="" # Database name
-mysqlcommand="mysql --user=$mysqluser --password=$mysqlpass"
+if [ "$1" == "" ]; then
+	echo "No database given"
+	exit
+fi
+mysqluser="root" # User to create database with
+mysqlwebuser="forensics" # Username for php user
+mysqlwebpass="A15j89%%8JsTk991LexzQ#" # Password for php user
+mysqldbname=$1 # Database name
+mysqlcommand="mysql --user=$mysqluser -p"
 $mysqlc -e "create database $mysqldbname;"
 if [ "$?" != "0" ]; then
 	echo -en "Error creating database $mysqldbname. Continue anyway (y/n)? "
@@ -21,14 +24,14 @@ if [ "$?" != "0" ]; then
 		exit
 	fi
 fi
-$mysqlc --database=$mysqldbname -e "create user '"$mysqlwebuser"'@'localhost' identified by '"$mysqlwebpass"';"
-if [ "$?" != "0" ]; then
-	echo -en "Error creating web user. Continue anyway (y/n)? "
-	read continuevar
-	if [ "$continuevar" != "y" ]; then
-		exit
-	fi
-fi
+#$mysqlc --database=$mysqldbname -e "create user '"$mysqlwebuser"'@'localhost' identified by '"$mysqlwebpass"';"
+#if [ "$?" != "0" ]; then
+#	echo -en "Error creating web user. Continue anyway (y/n)? "
+#	read continuevar
+#	if [ "$continuevar" != "y" ]; then
+#		exit
+#	fi
+#fi
 $mysqlc --database=$ysqldbname -e "grant all on $mysqldbname.* to '"$mysqlwebuser"'@'localhost';"
 if [ "$?" != "0" ]; then
 	echo -en "Error granting web user permissions. Continue anyway (y/n)? "
