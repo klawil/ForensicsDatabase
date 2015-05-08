12a60a50a32d51f41635e7b7ab18cbd9d1461b27
klawil <wklausmeyer@yahoo.com>
2015-05-07 20:54:09 -0500

    Removed foreach loop

--- index.php
+++ index.php
@@ -4,10 +4,5 @@ $GLOBALS['PageName'] = $GLOBALS['DBData']['SchoolName'];
 require_once 'header.inc';
 ?>
 <h1>Site under construction</h1>
-<?php
-foreach ( $GLOALS['DBData'] as $Key => $Value ) {
-	echo $Key . '=>' . $Value . '<br>';
-}
-?>
 </body>
 </html>
