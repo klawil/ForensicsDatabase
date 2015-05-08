7a9952da7994e7932bb49c35528a46c0f3320c97
klawil <wklausmeyer@yahoo.com>
2015-05-07 20:51:54 -0500

    Added some stuff

--- ResultInsert.php
+++ ResultInsert.php
@@ -5,6 +5,23 @@ require_once 'restrictedpage.inc';
 
 $ErrorString = '';
 
+if ( isset($_POST['TournamentID']) ) {do {
+	// Set TournamentID and validate it
+	$TournamentID = MySQLEscape($_POST['TournamentID']);
+	if ( !IsID($DBConn,$TournamentID,'Tournaments') ) {
+		$ErrorString = 'Invalid tournament';
+		break;
+	}
+	
+	// Get Tournament Data
+	$TournamentQuery = MySQLQuery($DBConn,'select TournamentName, NumRounds, NumJudges, NumElimRounds, NumElimJudges from Tournaments where TournamentID="' . $TournamentID . '";');
+	if ( !$TournamentQuery['Result'] ) {
+		$ErrorString = $TournamentQuery['Result'];
+		break;
+	}
+	$TournamentData = mysqli_fetch_assoc($TournamentQuery['Query']);
+} while(false); }
+
 require_once 'header.inc';
 ?>
 <style>
@@ -12,14 +29,37 @@ input[type=number] {
 	width: 3em;
 }
 </style>
-<h3>Insert results from a tournament</h3>
+<?php if ( isset($TournamentData['TournamentName']) ) {?>
+<h2><?php echo $TournamentData['TournamentName']; ?></h2>
+<?php } ?>
+<h3>Insert results from tournament</h3>
 <div id="PostMessage" class="ErrorMessage"><?php echo $ErrorString;?></div>
 <?php if ( !isset($_POST['TournamentID']) ) { ?>
 <form id="TournamentSelect" action="ResultInsert.php" method="post">
 Select a Tournament: <?php echo CreateList($DBConn, 'Tournaments');?> <input type="submit" value="Select Tournament">
 </form>
-<?php } else {
-	echo $_POST['TournamentID'];
-}?>
+<?php
+} else {
+?>
+<form id="ResultForm" target="ResultInsert.php" method="post">
+Student: <?php echo CreateList($DBConn,'Students'); ?><br>
+<div id="PartnerSelect" class="hidden">Partner: <?php echo CreateList($DBConn,'Students',NULL,NULL,'PartnerID'); ?><br></div>
+Event: <?php echo CreateList($DBConn,'Events'); ?><br>
+<table class="Table">
+<tr>
+	<th></th><?php for ( $Judge = 1; $Judge <= $TournamentData['NumJudges']; $Judge++ ) {?><th colspan="2">Judge <?php echo $Judge; ?></th><?php} ?>
+</tr>
+<?php
+	for ( $Round = 1; $Round <= $TournamentData['NumRounds']; $Round++ ) {
+?>
+<tr>
+	<td>Round <?php echo $Round; ?></td><?php for ( $Judge = 1; $Judge <= $TournamentData['NumJudges']; $Judge++ ) {?><td><input type="number" id="Round[<?php echo $Round; ?>][<?php echo $Judge; ?>]['Rank']"></td><td><input type="number" id="Round[<?php echo $Round; ?>][<?php echo $Judge; ?>]['Qual']"></td><?php }?>
+</tr>
+<?php
+	}
+}
+?>
+</table>
+</form>
 </body>
 </html>
