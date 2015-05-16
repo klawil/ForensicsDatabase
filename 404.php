2b2bc8089343938c44c2bd6dc8ae28bf2e166f99
klawil <wklausmeyer@yahoo.com>
2015-05-16 12:34:16 -0500

    Changed path of picture to be absolute

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ require_once 'header.inc';
 ?>
 <center>
 <h1>Oops! You've caught us speechless!</h1>
-<img src="404.png" alt="This was a really cool meme!" style="width:100%;max-width:636px;padding-top:20px">
+<img src="/404.png" alt="This was a really cool meme!" style="width:100%;max-width:636px;padding-top:20px">
 <h3>We can't find that page on our server.<br>Must be a bad link!<br>
 <?php if ( strpos($_SERVER['HTTP_REFERER'],'forensicsdb') ) { echo '<a href="' . $_SERVER['HTTP_REFERER'] . '">Go Back</a> or '; } ?>
 Try <a href="http://forensicsdb.com">This One</a> instead!</h3>
