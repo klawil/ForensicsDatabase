47dd0b1079ddafcab0edb4de607a9edfada7eb90
klawil <wklausmeyer@yahoo.com>
2015-05-17 22:26:18 -0500

    Deleted duplicate js function

--- ManageSeason.php
+++ ManageSeason.php
@@ -220,47 +220,6 @@ function DeleteSeason(SeasonID) {
 	// Execute Post
 	PostToPage(PostString,"ManageSeason.php","PostMessage");
 }
-
-function GetChange(SeasonID) {
-	// See if there is a change and if so show the save changes button
-	
-	// Create array to guide checking
-	var ChangeArray = ["SeasonName","StartYear"];
-	
-	// Initialize variable to check for changes
-	var ischange = false;
-
-	// Loop through the change array and find a change
-	for ( var Index = 0; Index < ChangeArray.length; Index++ ) {
-		// Set the information needed
-		var ElementID = ChangeArray[Index] + SeasonID;
-		var Element = document.getElementById(ElementID);
-		switch ( Element.type ) {
-			case "checkbox":
-				if ( Element.checked != Element.defaultChecked ) {
-					ischange = true;
-				}
-				break;
-			case "select":
-				if ( !Element.options[Element.selectedIndex].defaultSelected ) {
-					ischange = true;
-				}
-				break;
-			default:
-				if ( Element.defaultValue != Element.value ) {
-					ischange = true;
-				}
-		}
-	}
-	
-	// If there is change, show the button, otherwise hide it
-	if ( ischange ) {
-		var displaytype = "inline";
-	} else {
-		var displaytype = "none";
-	}
-	document.getElementById("ChangeCell" + SeasonID).style.display = displaytype;
-}
 </script>
 </body>
 </html>
